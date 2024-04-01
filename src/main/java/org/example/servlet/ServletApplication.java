e104c135d1476f1700abaefede307ad5781e2744
김다은 <sss3598@gmail.com>
2024-04-01 21:34:16 +0900

    서블릿 프로젝트 생성

---
+++ src/main/java/org/example/servlet/ServletApplication.java
@@ -0,0 +1,13 @@
+package org.example.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServletApplication.class, args);
+  }
+
+}
