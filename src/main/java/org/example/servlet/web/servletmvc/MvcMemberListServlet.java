da05eab8a7a779ee0f984b5c1c0494da9cb9c97a
김다은 <sss3598@gmail.com>
2024-04-03 23:38:29 +0900

    MVC패턴 적용하여 회원 등록 및 회원 조회 개발

---
+++ src/main/java/org/example/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,31 @@
+package org.example.servlet.web.servletmvc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import org.example.servlet.domain.member.Member;
+import org.example.servlet.domain.member.MemberRepository;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+  MemberRepository memberRepository = MemberRepository.getInstance();
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    List<Member> members = memberRepository.findAll();
+
+    request.setAttribute("members", members);
+
+    String viewPath = "/WEB-INF/views/members.jsp";
+    RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+    dispatcher.forward(request, response);
+  }
+}
