714804584ae7ba025f6e14e5c402dbfe0b5d0415
김다은 <sss3598@gmail.com>
2024-04-11 20:48:37 +0900

    상품 등록 - @ModelAttribute

--- src/main/java/org/example/itemservice/web/basic/BasicItemController.java
+++ src/main/java/org/example/itemservice/web/basic/BasicItemController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -38,9 +39,31 @@ public class BasicItemController {
     return "basic/addForm";
   }
 
+//  @PostMapping("/add")
+  public String addItemV1(
+      @RequestParam String itemName,
+      @RequestParam Integer price,
+      @RequestParam Integer quantity,
+      Model model) {
+
+    Item item = new Item();
+    item.setItemName(itemName);
+    item.setPrice(price);
+    item.setQuantity(quantity);
+
+    itemRepository.save(item);
+
+    model.addAttribute("item", item);
+
+    return "basic/item";
+  }
+
   @PostMapping("/add")
-  public String save() {
-    return "basic/addForm";
+  public String addItemV2(Item item) {
+    itemRepository.save(item);
+//    model.addAttribute("item", item);
+
+    return "basic/item";
   }
 
   @PostConstruct
