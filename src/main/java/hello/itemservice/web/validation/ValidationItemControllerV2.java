2772e9a9a776a5d332f80f48f63b0041ef281f39
daeun <sss3598@gmail.com>
2024-04-15 22:23:23 +0900

    V2: 비즈니스 로직과 validator 분리

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -25,6 +25,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class ValidationItemControllerV2 {
 
   private final ItemRepository itemRepository;
+  private final ItemValidator itemValidator;
 
   @GetMapping
   public String items(Model model) {
@@ -168,7 +169,6 @@ public class ValidationItemControllerV2 {
     return "redirect:/validation/v2/items/{itemId}";
   }
 
-  @PostMapping("/add")
   public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult,
       RedirectAttributes redirectAttributes) {
 
@@ -199,6 +199,25 @@ public class ValidationItemControllerV2 {
         bindingResult.reject("totalPriceMain", new Object[]{10000, resultPrice}, null);
       }
     }
+
+    //검증 실패 -> 다시 입력 폼으로 이동
+    if (bindingResult.hasErrors()) {
+      log.info("errors={}", bindingResult);
+      return "/validation/v2/addForm";
+    }
+
+    //성공 로직
+    Item savedItem = itemRepository.save(item);
+    redirectAttributes.addAttribute("itemId", savedItem.getId());
+    redirectAttributes.addAttribute("status", true);
+    return "redirect:/validation/v2/items/{itemId}";
+  }
+
+  @PostMapping("/add")
+  public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
+      RedirectAttributes redirectAttributes) {
+
+    itemValidator.validate(item, bindingResult);
 
     //검증 실패 -> 다시 입력 폼으로 이동
     if (bindingResult.hasErrors()) {
