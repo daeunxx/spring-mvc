4e0bb0f3c9020b6cd8ab846a38747d5fcde003c2
daeun <sss3598@gmail.com>
2024-04-16 20:18:56 +0900

    V3: ObjectError 적용

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -7,27 +7,28 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합은 10,000원 이상이 되어야 합니다.")
 public class Item {
 
-    private Long id;
+  private Long id;
 
-    @NotBlank
-    private String itemName;
+  @NotBlank
+  private String itemName;
 
-    @NotNull
-    @Range(min=1000, max=1000000)
-    private Integer price;
+  @NotNull
+  @Range(min = 1000, max = 1000000)
+  private Integer price;
 
-    @NotNull
-    @Max(9999)
-    private Integer quantity;
+  @NotNull
+  @Max(9999)
+  private Integer quantity;
 
-    public Item() {
-    }
+  public Item() {
+  }
 
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
+  public Item(String itemName, Integer price, Integer quantity) {
+    this.itemName = itemName;
+    this.price = price;
+    this.quantity = quantity;
+  }
 }
