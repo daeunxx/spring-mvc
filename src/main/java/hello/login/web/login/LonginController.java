4e59501df7fdf422b2c35456c9b91b83a79a8f4e
daeun <sss3598@gmail.com>
2024-04-19 08:51:43 +0900

    로그인 처리 - 쿠키 사용

--- src/main/java/hello/login/web/login/LonginController.java
+++ src/main/java/hello/login/web/login/LonginController.java
@@ -2,6 +2,8 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -24,7 +26,8 @@ public class LonginController {
   }
 
   @PostMapping("/login")
-  public String login(@Validated LoginForm form, BindingResult bindingResult) {
+  public String login(@Validated LoginForm form, BindingResult bindingResult,
+      HttpServletResponse response) {
 
     if (bindingResult.hasErrors()) {
       return "login/loginForm";
@@ -37,8 +40,24 @@ public class LonginController {
       return "login/loginForm";
     }
 
-    // TODO: 로그인처리
+    //로그인 성공 처리
+
+    //쿠키에 시간 정보를 주지 않으면 자동으로 세션 쿠키(브라우저 종료 시 모두 종료)가 됨
+    Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+    response.addCookie(idCookie);
 
     return "redirect:/";
+  }
+
+  @PostMapping("logout")
+  public String logout(HttpServletResponse response) {
+    expireCookie(response, "memberId");
+    return "redirect:/";
+  }
+
+  private static void expireCookie(HttpServletResponse response, String cookieName) {
+    Cookie cookie = new Cookie(cookieName, null);
+    cookie.setMaxAge(0);
+    response.addCookie(cookie);
   }
 }
