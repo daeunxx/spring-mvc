5f5f976d3cf8e1e2ccda3938b433f491dac5e15c
daeun <sss3598@gmail.com>
2024-04-21 13:15:27 +0900

    ArgumentResolver 적용

--- src/main/java/hello/login/web/WebConfig.java
+++ src/main/java/hello/login/web/WebConfig.java
@@ -1,12 +1,15 @@
 package hello.login.web;
 
+import hello.login.web.argumentresolver.LoginMemberArguementResolver;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
 import hello.login.web.interceptor.LoginCheckInterceptor;
 import jakarta.servlet.Filter;
+import java.util.List;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -47,5 +50,10 @@ public class WebConfig implements WebMvcConfigurer {
         .addPathPatterns("/**")
         .excludePathPatterns("/", "/members/add", "/login", "/logout",
             "/css/**", "/*.ico", "/error");
+  }
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(new LoginMemberArguementResolver());
   }
 }
