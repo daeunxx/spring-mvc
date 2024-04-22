b7ded0643de92a2a0ebfbeccb9f42c77d31591d3
daeun <sss3598@gmail.com>
2024-04-22 21:31:25 +0900

    타입컨버터 프로젝트 생성

---
+++ src/test/java/org/example/typeconverter/TypeconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeconverterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
