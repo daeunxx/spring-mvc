d61a747c5e18c59da0995049a8a0edd62d86afed
daeun <sss3598@gmail.com>
2024-04-23 21:04:14 +0900

    WebConfig addConverter 적용

---
+++ src/main/java/org/example/typeconverter/WebConfig.java
@@ -0,0 +1,21 @@
+package org.example.typeconverter;
+
+import org.example.typeconverter.converter.IntegerToStringConverter;
+import org.example.typeconverter.converter.IpPortToStringConverter;
+import org.example.typeconverter.converter.StringToIntegerConverter;
+import org.example.typeconverter.converter.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(new StringToIntegerConverter());
+    registry.addConverter(new IntegerToStringConverter());
+    registry.addConverter(new StringToIpPortConverter());
+    registry.addConverter(new IpPortToStringConverter());
+  }
+}
