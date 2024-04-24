9e55492998df36dc023cb5e22b68a7b7baf0a20d
daeun <sss3598@gmail.com>
2024-04-24 23:00:09 +0900

    스프링 기본 컨버터

--- src/main/java/org/example/typeconverter/WebConfig.java
+++ src/main/java/org/example/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import org.example.typeconverter.converter.IntegerToStringConverter;
 import org.example.typeconverter.converter.IpPortToStringConverter;
 import org.example.typeconverter.converter.StringToIntegerConverter;
 import org.example.typeconverter.converter.StringToIpPortConverter;
+import org.example.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addFormatters(FormatterRegistry registry) {
-    registry.addConverter(new StringToIntegerConverter());
-    registry.addConverter(new IntegerToStringConverter());
+    //주석처리 우선순위
+//    registry.addConverter(new StringToIntegerConverter());
+//    registry.addConverter(new IntegerToStringConverter());
     registry.addConverter(new StringToIpPortConverter());
     registry.addConverter(new IpPortToStringConverter());
+
+    //추가
+    registry.addFormatter(new MyNumberFormatter());
   }
 }
